1d931c8846f25dff0bf4fcc4e1af47cc2bb644c1
maddylam <maddylam04@gmail.com>
2024-03-26 01:54:04 -0700

    made a show password functionality

--- group/src/main/resources/static/stylesheets/settings.js
+++ group/src/main/resources/static/stylesheets/settings.js
@@ -7,3 +7,16 @@ function showChangePassword(){
     document.getElementById("change-pass-page").style.display = "block"
     document.getElementById("main-options").style.display = "none"
 }
+
+function showpass(){
+    var oldpass = document.getElementById("oldpass")
+    var newpass = document.getElementById("newpass")
+
+    if (oldpass.type === "password" && newpass.type === "password") {
+        oldpass.type = "text";
+        newpass.type = "text";
+      } else {
+        oldpass.type = "password";
+        newpass.type = "password";
+      }
+}
